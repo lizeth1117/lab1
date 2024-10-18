78cc1ec3fa947089f968ee70f45f8cf56f546cb5
lizeth1117 <reynamlndz@hotmail.com>
2024-10-18 08:19:51 -0700

    inicio

---
+++ src/reyna_melendez_lab1/Reyna_melendez_lab1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package reyna_melendez_lab1;
+
+/**
+ *
+ * @author lizet
+ */
+public class Reyna_melendez_lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola");
+    }
+    
+}
